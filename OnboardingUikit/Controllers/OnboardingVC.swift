0d94b04f00b3c665fa72afef2f24db3a82e3cd0b
Taha Jadid <jadid.taha@waroom.com>
2023-02-20 10:24:46 +0100

    First Commit

---
+++ OnboardingUikit/Controllers/OnboardingVC.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingVC.swift
+//  OnboardingUikit
+//
+//  Created by Taha JADID on 20/2/2023.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
